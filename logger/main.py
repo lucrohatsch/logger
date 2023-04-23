0832ad0bccb8aa87919e881c378a112e10ac13a4
lucrohatsch <lucas.rohatsch@gamil.com>
2023-04-22 21:04:02 -0300

    config stdout and format

--- logger/main.py
+++ logger/main.py
@@ -1,17 +1,19 @@
 import logging
 from logger.enums import lavels
 from datetime import datetime
+import sys
 
 
 
 class Logger():
     def __init__(self, lavel='debug'):
         self.lavel = lavels.get(lavel.lower())
-        logging.basicConfig(level=self.lavel)
+        logging.basicConfig(level=self.lavel, format='%(message)s', stream=sys.stdout)
+        
         msg = {
             "lavel":"info",
             "time": str(datetime.now()),
-            "message": f"Starting logger in {lavel.upper} mode"
+            "message": f"Starting logger in {lavel.upper()} mode"
         }
         logging.info(msg)        
 
@@ -23,6 +25,14 @@ class Logger():
     def error(self, message):
         msg = self._format_message(message)
         logging.error(msg)
+    
+    def warning(self, message):
+        msg = self._format_message(message)
+        logging.warning(msg)
+
+    def metrics(self, name, value):
+        msg = self._format_metric(name, value)
+        logging.info(msg)
 
     def _format_message(self, message:any) -> dict:
         return {
@@ -31,3 +41,11 @@ class Logger():
             "message": str(message)
         }
 
+    def _format_metric(self, name, value) -> dict:
+        return {
+            "lavel":f"{self.lavel}",
+            "time": str(datetime.now()),
+            "metric": {
+                name: value 
+            } 
+        }
